d619582ddfd024a331318894fb10f03f0b5d1b98
claudemotoba <claudemotoba933@gmail.com>
2023-11-23 19:40:38 +0100

    ajout des info la-maison-jungle

---
+++ index.js
@@ -0,0 +1,28 @@
+function Banner() {
+    const title = "La maison jungle"
+      return (<h1>{title.toUpperCase()}</h1>)
+    }
+    
+    function Cart() {
+    const monsteraPrice = 8
+    const ivyPrice = 10
+    const flowerPrice = 15
+    return (<div>
+        <h2>Panier</h2>
+        <ul>
+            <li>Monstera : {monsteraPrice}€</li>
+            <li>Lierre : {ivyPrice}€</li>
+            <li>Fleurs : {flowerPrice}€</li>
+        </ul>
+          Total : {monsteraPrice + ivyPrice + flowerPrice }€
+          </div>)
+    }
+    
+
+
+ReactDOM.render(<React.Fragment>
+    <Banner />
+    <Cart />
+    </React.Fragment>,
+    document.getElementById("root")
+)
